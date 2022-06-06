d5211eb7f401da7bd832bd2a44f04cc1f3f974de
Deivid Vale <deividrodriguesvale@gmail.com>
2022-06-06 14:21:16 +0200

    Reorganized term.mli into sections and added commit info.

--- src/lib/syntax/term.mli
+++ src/lib/syntax/term.mli
@@ -1,4 +1,11 @@
-(** OCaml type for sorts *)
+(**
+  {0 {b Module} Term}
+*)
+
+(*--------------------------------------------------------------------
+  Sort and Type
+--------------------------------------------------------------------*)
+
 type sort
 
 val sort_equal : sort -> sort -> bool
@@ -16,9 +23,17 @@ val typ_ins : typ -> sort list
 val typ_out : typ -> sort
 
 val typ_equal : typ -> typ -> bool
+(**
+  [type_equal typ typ'] is whether [typ] and [typ']
+  are structurally equal.
+*)
 
 val typ_is_sort : typ -> bool
 (** [type_is_sort typ] is whether [typ] is a sort. *)
+
+(*--------------------------------------------------------------------
+  Function Symbols
+--------------------------------------------------------------------*)
 
 type func
 
@@ -31,6 +46,14 @@ val func_of_string_opt : string -> func option
 val func_symbolize : (func -> unit) -> string -> func
 
 val func_to_string : func -> string
+
+val func_set_typ : func -> typ -> unit
+
+val func_get_typ : func -> typ
+
+(*--------------------------------------------------------------------
+  Variable
+--------------------------------------------------------------------*)
 
 type var
 (** OCaml type for variables *)
@@ -49,13 +72,13 @@ val var_symbolize : (var -> unit) -> string -> var
 
 val var_to_string : var -> string
 
-val func_set_typ : func -> typ -> unit
-
-val func_get_typ : func -> typ
-
 val var_set_sort : var -> sort -> unit
 
 val var_get_sort : var -> sort
+
+(*--------------------------------------------------------------------
+  Term Syntax
+--------------------------------------------------------------------*)
 
 type sym = F of func | V of var
 

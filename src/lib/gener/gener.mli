a75c6cee4776c3759610af0eede51e66d8106e01
Liye Guo <guo.ly@hotmail.com>
2023-06-23 21:42:46 +0200

    Add bound checks for undetermined coefficients.

--- src/lib/gener/gener.mli
+++ src/lib/gener/gener.mli
@@ -14,5 +14,8 @@ val gener_v : (sort -> int) -> int -> var -> cost * size
 (** Assertion generation for a single rewrite rule. *)
 val gener_r : (func -> cost * size) -> (var -> cost * size) -> term * term -> Z3env.t -> Z3.Expr.expr list
 
-(** Assertion of an undetermined coefficient's nonnegativity. *)
-val undet_nonneg : int -> Z3env.t -> Z3.Expr.expr
+(** Assertion of an undetermined coefficient's lower bound. *)
+val undet_ge : int -> int -> Z3env.t -> Z3.Expr.expr
+
+(** Assertion of an undetermined coefficient's upper bound. *)
+val undet_le : int -> int -> Z3env.t -> Z3.Expr.expr
